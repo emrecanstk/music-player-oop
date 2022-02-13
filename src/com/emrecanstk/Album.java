528c646bf7f233986ed77fd87a7a73a3e7fb32df
Emre Can Satık <emrecanstk@gmail.com>
2022-02-13 22:03:51 +0300

    add a constructor

--- src/com/emrecanstk/Album.java
+++ src/com/emrecanstk/Album.java
@@ -7,5 +7,9 @@ public class Album {
     private String artist;
     private ArrayList<Song> songs;
 
-    
+    public Album(String name, String artist, ArrayList<Song> songs) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = songs;
+    }
 }
