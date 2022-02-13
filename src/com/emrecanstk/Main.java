927daa080b04ea7950fbe84287de4b0dc2df4617
Emre Can Satık <emrecanstk@gmail.com>
2022-02-13 23:34:38 +0300

    play list adding codes

--- src/com/emrecanstk/Main.java
+++ src/com/emrecanstk/Main.java
@@ -15,16 +15,24 @@ public class Main {
         album.addSong("Koy",4.2);
         albums.add(album);
 
-        Album album2 = new Album("Mancoloji","Baris Manco");
+        album = new Album("Mancoloji","Baris Manco");
         album.addSong("Hal Hal",3.7);
         album.addSong("Gul Pembe",3.4);
         album.addSong("Nazar Eyle",4.5);
         album.addSong("Donence",3.9);
         album.addSong("Halil Ibrahim Sofrasi",3.2);
-        albums.add(album2);
+        albums.add(album);
+
+        LinkedList<Song> playList_1 = new LinkedList<>();
+        albums.get(0).addToPlayList("Milimetrik Atis",playList_1);
+        albums.get(0).addToPlayList("Koy",playList_1);
+        albums.get(1).addToPlayList("Donence",playList_1);
+        albums.get(1).addToPlayList("Hal Hal",playList_1);
+
+        play(playList_1);
+
     }
 
-    LinkedList<Song> playlist_1 = new LinkedList<>();
-    
+
 
 }
