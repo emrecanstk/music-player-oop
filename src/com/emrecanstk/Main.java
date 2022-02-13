0874b1f817e2c06f4266650ead8bb11f6feeb861
Emre Can Satık <emrecanstk@gmail.com>
2022-02-13 22:37:32 +0300

    add two albums and their songs

--- src/com/emrecanstk/Main.java
+++ src/com/emrecanstk/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Album album = new Album("Album1","Eray Uraz");
+        album.addSong("Milimetrik Atis",3.5);
+        album.addSong("Tam Kafadan",3.11);
+        album.addSong("Koy",4.2);
+
+        Album album2 = new Album("Mancoloji","Baris Manco");
+        album.addSong("Hal Hal",3.7);
+        album.addSong("Gul Pembe",3.4);
+        album.addSong("Nazar Eyle",4.5);
+        album.addSong("Donence",3.9);
+        album.addSong("Halil Ibrahim Sofrasi",3.2);
     }
 }
