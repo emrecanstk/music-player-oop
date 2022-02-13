ad6b6f4c3bdb5abb899ef5f7ef06e4978c1abfb7
Emre Can Satık <emrecanstk@gmail.com>
2022-02-13 22:37:13 +0300

    some word edits

--- src/com/emrecanstk/Album.java
+++ src/com/emrecanstk/Album.java
@@ -8,7 +8,7 @@ public class Album {
     private String artist;
     private ArrayList<Song> songs;
 
-    public Album(String name, String artist, ArrayList<Song> songs) {
+    public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
         this.songs = new ArrayList<Song>();
