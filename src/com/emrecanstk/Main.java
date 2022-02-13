1b9d502e7060a0ae492511891de796cf1bae2262
Emre Can Satık <emrecanstk@gmail.com>
2022-02-13 23:05:40 +0300

    update main.java for lists

--- src/com/emrecanstk/Main.java
+++ src/com/emrecanstk/Main.java
@@ -1,6 +1,11 @@
 package com.emrecanstk;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
+
+    private static ArrayList<Album> albums = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -8,6 +13,7 @@ public class Main {
         album.addSong("Milimetrik Atis",3.5);
         album.addSong("Tam Kafadan",3.11);
         album.addSong("Koy",4.2);
+        albums.add(album);
 
         Album album2 = new Album("Mancoloji","Baris Manco");
         album.addSong("Hal Hal",3.7);
@@ -15,5 +21,10 @@ public class Main {
         album.addSong("Nazar Eyle",4.5);
         album.addSong("Donence",3.9);
         album.addSong("Halil Ibrahim Sofrasi",3.2);
+        albums.add(album2);
     }
+
+    LinkedList<Song> playlist_1 = new LinkedList<>();
+    
+
 }
